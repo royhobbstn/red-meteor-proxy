2b49600e65753fbc3e8303167c7a3e2fe0a0f38f
royhobbstn <royhobbstn@yahoo.com>
2016-11-06 16:54:58 -0500

    last commit doesnt work.  this does

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 
+
+//redirect http to https
+
+// http://heyrod.com/snippets/redirect-http-to-https-in-expressjs.html
+var http = require('http');
+var express = require('express');
+var HTTP_PORT  = 3501;
+var HTTPS_PORT = 443;
+
+var http_app = express();
+http_app.set('port', HTTP_PORT);
+
+http_app.all('/*', function(req, res, next) {
+  if (/^http$/.test(req.protocol)) {
+    var host = req.headers.host.replace(/:[0-9]+$/g, ""); // strip the port # if any
+    if ((HTTPS_PORT != null) && HTTPS_PORT !== 443) {
+      return res.redirect("https://" + host + ":" + HTTPS_PORT + req.url, 301);
+    } else {
+      return res.redirect("https://" + host + req.url, 301);
+    }
+  } else {
+    return next();
+  }
+});
+
+
+http.createServer(http_app).listen(HTTP_PORT).on('listening', function() {
+  return console.log("HTTP to HTTPS redirect app launched.");
+});
+
+
 // redbird
 
 
 var redbird = require('redbird')({ port: 3500 });
-redbird.register('www.red-meteor.com/mlb-map', 'http://0.0.0.0:4000');
+redbird.register('www.red-meteor.com/mlb', 'http://0.0.0.0:4000');
 
