9c4692c350bdf14326287fa4dfecd2ec8e1a8ea5
Daniel Trone <royhobbstn@yahoo.com>
2017-02-02 13:12:52 +0000

    hmm just redbird

--- index.js
+++ index.js
@@ -1,39 +1,3 @@
-//redirect http to https
-
-// http://heyrod.com/snippets/redirect-http-to-https-in-expressjs.html
-var http = require('http');
-var express = require('express');
-var HTTP_PORT = 80;
-var HTTPS_PORT = 443;
-
-var http_app = express();
-http_app.set('port', HTTP_PORT);
-
-
-http_app.all('/*', function (req, res, next) {
-  if (/^http$/.test(req.protocol)) {
-    var host = req.headers.host.replace(/:[0-9]+$/g, ""); // strip the port # if any
-    if ((HTTPS_PORT != null) && HTTPS_PORT !== 443) {
-      return res.redirect("https://" + host + ":" + HTTPS_PORT + req.url, 301);
-    }
-    else {
-      return res.redirect("https://" + host + req.url, 301);
-    }
-  }
-  else {
-    return next();
-  }
-});
-
-
-
-
-http.createServer(http_app).listen(HTTP_PORT).on('listening', function () {
-  return console.log("HTTP to HTTPS redirect app launched.");
-});
-
-
-
 // Redbird
 
 
@@ -64,4 +28,6 @@ redbird.register('red-meteor.com/censusapi', 'http://red-meteor.com:4002', {
 });
 
 
-redbird.register("https://red-meteor.com", "https://maputopia.com/portfolio");
+redbird.register("red-meteor.com", "https://maputopia.com/portfolio", {
+  ssl: true
+});
